7bc0c89ea9a4f4614ebc9c4afa270664ff8018aa
ragingrahul <raj17sarma@gmail.com>
2023-03-18 23:49:27 +0530

    uniswap

---
+++ uniswap/swap.js
@@ -0,0 +1,4 @@
+import {ethers} from 'ethers'
+import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
+import SwapRouterABI from '@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json'
+import ERC20ABI from './abi.json'
