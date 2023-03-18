126c38efb22d9801e9cb5e3950d3620be1920777
ragingrahul <raj17sarma@gmail.com>
2023-03-19 01:08:33 +0530

    uniswap v0.1

--- uniswap/swap.js
+++ uniswap/swap.js
@@ -1,4 +1,10 @@
-import {ethers} from 'ethers'
-import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json'
-import SwapRouterABI from '@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json'
-import ERC20ABI from './abi.json'
+const {ethers} = require('ethers')
+const {abi:IUniswapV3PoolABI} =require('@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json')
+const {abi:SwapRouterABI} = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/ISwapRouter.sol/ISwapRouter.json')
+const ERC20ABI = require('./abi.json')
+require('dotenv').config()
+
+const ALCHEMY_URL_TESTNET=process.env.ALCHEMY_URL_TESTNET
+const WALLET_ADDRESS=process.env.WALLET_ADDRESS
+const WALLET_SECRET_KEY=process.env.WALLET_SECRET_KEY
+
