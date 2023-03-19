b3af5e49b3de4d4c6331dc81363cfe08f813d8b3
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-03-19 13:08:17 +0530

    Added QRCode and login/logout functionalities

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -4,29 +4,36 @@ import { Inter } from 'next/font/google'
 import { GaslessOnboarding, GaslessWalletConfig, LoginConfig} from '@gelatonetwork/gasless-onboarding';
 import { ethers } from 'ethers';
 import { useEffect, useState } from 'react';
+import { SafeEventEmitterProvider, UserInfo } from "@web3auth/base";
+import { GaslessWallet } from "@gelatonetwork/gasless-wallet";
+import QRCode from 'qrcode';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const gaslessWalletConfig = {
+  apiKey: process.env.NEXT_PUBLIC_ONEBALANCE_API_KEY,
+};
+const loginConfig = {
+  domains: ["http://localhost:3000"],
+  chain: {
+    id: 80001,
+    rpcUrl: process.env.NEXT_PUBLIC_ALCHEMY_RPC_URL,
+  },
+  openLogin: {
+    redirectUrl: "http://localhost:3000",
+  },
+};
 
-  useEffect(()=>{
-    login()
-  },[])
+export default function Home() {
+  const [gaslessOnboarding, setGaslessOnboarding] = useState<GaslessOnboarding>();
+  const [web3AuthProvider, setWeb3AuthProvider] = useState<SafeEventEmitterProvider>();
+  const [gaslessWallet, setGaslessWallet] = useState<GaslessWallet>();
+  const [address, setAddress] = useState("");
+  const [userInfo, setUserInfo] = useState<Partial<UserInfo> | null>();
+  const [qrCode, setQRCode] = useState<string | null>();
 
   const login = async() => {
-    try {
-      const gaslessWalletConfig = { apiKey: process.env.ONEBALANCE_API_KEY };
-      const loginConfig = {
-        domains: ["http://localhost:3000"],
-        chain: {
-          id: 80001,
-          rpcUrl: process.env.ALCHEMY_RPC_URL,
-        },
-        openLogin: {
-          redirectUrl: "http://localhost:3000",
-        },
-      };
-      
+    try {      
       const gaslessOnboarding = new GaslessOnboarding(
         loginConfig as LoginConfig,
         gaslessWalletConfig as GaslessWalletConfig
@@ -34,25 +41,44 @@ export default function Home() {
       await gaslessOnboarding.init();
 
       const web3AuthProvider = await gaslessOnboarding.login();
-      console.log(web3AuthProvider);
+      setWeb3AuthProvider(web3AuthProvider);
+      setGaslessOnboarding(gaslessOnboarding)
 
       const gaslessWallet = gaslessOnboarding.getGaslessWallet();
-      console.log(gaslessWallet);
+      setGaslessWallet(gaslessWallet);
 
       const address = gaslessWallet.getAddress();
-      console.log(address);
+      setAddress(address);
+      console.log(address)
+      generateQRCode(address);
 
       const userInfo = await gaslessOnboarding.getUserInfo();
-      console.log(userInfo);
+      setUserInfo(userInfo);
+
     } catch (error) {
       console.log(error);
     }
+  }
+
+  const generateQRCode = (address:String) => {
+    QRCode.toDataURL(address).then((url:string) => setQRCode(url))
+  }
+
+  const logout = async() => {
+    await gaslessOnboarding?.logout();
+
+    setGaslessOnboarding(undefined);
+    setWeb3AuthProvider(undefined);
+    setGaslessWallet(undefined);
+    setAddress("");
   }
 
   return (
     <>
       <div>
         <button onClick={login}> Login </button>
+        <button onClick={logout}> Logout </button>
+        {gaslessWallet && qrCode && <Image src = {qrCode} width={200} height={200} alt="QRCode" />}
       </div>
     </>
   )
