3faced3f275878723a537f3fbbad7e73694a1ff0
AnoyRC <38689344+AnoyRC@users.noreply.github.com>
2023-03-21 01:47:35 +0530

    Main Menu Started

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,14 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { GaslessOnboarding, GaslessWalletConfig, LoginConfig} from '@gelatonetwork/gasless-onboarding';
-import { ethers } from 'ethers';
-import { useEffect, useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import {
+  GaslessOnboarding,
+  GaslessWalletConfig,
+  LoginConfig,
+} from "@gelatonetwork/gasless-onboarding";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import { SafeEventEmitterProvider, UserInfo } from "@web3auth/base";
 import { GaslessWallet } from "@gelatonetwork/gasless-wallet";
-import QRCode from 'qrcode';
+import QRCode from "qrcode";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 const gaslessWalletConfig = {
   apiKey: process.env.NEXT_PUBLIC_ONEBALANCE_API_KEY,
@@ -25,15 +29,17 @@ const loginConfig = {
 };
 
 export default function Home() {
-  const [gaslessOnboarding, setGaslessOnboarding] = useState<GaslessOnboarding>();
-  const [web3AuthProvider, setWeb3AuthProvider] = useState<SafeEventEmitterProvider>();
+  const [gaslessOnboarding, setGaslessOnboarding] =
+    useState<GaslessOnboarding>();
+  const [web3AuthProvider, setWeb3AuthProvider] =
+    useState<SafeEventEmitterProvider>();
   const [gaslessWallet, setGaslessWallet] = useState<GaslessWallet>();
   const [address, setAddress] = useState("");
   const [userInfo, setUserInfo] = useState<Partial<UserInfo> | null>();
   const [qrCode, setQRCode] = useState<string | null>();
 
-  const login = async() => {
-    try {      
+  const login = async () => {
+    try {
       const gaslessOnboarding = new GaslessOnboarding(
         loginConfig as LoginConfig,
         gaslessWalletConfig as GaslessWalletConfig
@@ -42,44 +48,67 @@ export default function Home() {
 
       const web3AuthProvider = await gaslessOnboarding.login();
       setWeb3AuthProvider(web3AuthProvider);
-      setGaslessOnboarding(gaslessOnboarding)
+      setGaslessOnboarding(gaslessOnboarding);
 
       const gaslessWallet = gaslessOnboarding.getGaslessWallet();
       setGaslessWallet(gaslessWallet);
 
       const address = gaslessWallet.getAddress();
       setAddress(address);
-      console.log(address)
+      console.log(address);
       generateQRCode(address);
 
       const userInfo = await gaslessOnboarding.getUserInfo();
       setUserInfo(userInfo);
-
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
-  const generateQRCode = (address:string) => {
-    QRCode.toDataURL(address).then((url:string) => setQRCode(url))
-  }
+  const generateQRCode = (address: string) => {
+    QRCode.toDataURL(address).then((url: string) => setQRCode(url));
+  };
 
-  const logout = async() => {
+  const logout = async () => {
     await gaslessOnboarding?.logout();
 
     setGaslessOnboarding(undefined);
     setWeb3AuthProvider(undefined);
     setGaslessWallet(undefined);
     setAddress("");
-  }
+  };
 
   return (
-    <>
-      <div>
-        <button onClick={login}> Login </button>
-        <button onClick={logout}> Logout </button>
-        {gaslessWallet && qrCode && <Image src = {qrCode} width={200} height={200} alt="QRCode" />}
+    <div>
+      <div className="bg-[#191919] h-screen w-screen">
+        <div className="absolute top-0 left-0 h-[100vh] w-[100vw] landing-page-1 z-0"></div>
+        <div className="flex z-20 relative">
+          <div className="w-[70vw]">
+            <div className="ml-[5vw] mt-[25vh]">
+              <h1 className="text-[#ffffff] font-[Cotta] text-9xl">
+                Join the new era of Digital Finance
+              </h1>
+              <h1 className="text-[#ffffff] font-[Sarabun] mt-10 text-2xl">
+                Buying, trading & earning crypto was never been easier
+              </h1>
+              <div className="transition ease-linear duration-300 bg-[#191919] rounded-lg text-[#ffffff] hover:text-[#191919] p-3 px-4 w-fit mt-10 border-[#06f2a8] hover:bg-[#06f2a8] hover:cursor-pointer border-[1px]">
+                <h1 className="font-[Sarabun] text-lg font-bold">
+                  Get Started
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className="w-[30vw] flex">
+            <Image
+              src="/CoinRender.png"
+              height={1080}
+              width={1920}
+              alt="BgImage"
+              className="mt-[25vh] w-[50vw] max-w-[100vw] -left-[15vw] relative"
+            />
+          </div>
+        </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
